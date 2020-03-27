279c8ed3aa9fc20fc998b6829d36e3d2e169e985
shinKihan <kshin@ecaresoft.com>
2020-03-27 09:57:31 -0600

    feautre/add js to count buzzfizz

---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,20 @@
+
+let userInput = document.querySelector('.userInput')
+
+function knowType(){
+
+}
+
+function begining() {
+  
+} 
+
+function checkItem() {
+  let children = [];
+  for(let i=0; i<=num; i++) {
+    let isFizz = i%3 == 0
+    let isBuzz = i%5 == 0
+
+    children.push(fuction bring what type it is)
+  } 
+}
